60b2604e0935dd91d8af6a98f766a29cbc116230
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-31 18:25:54 +0900

    Update MemberServicelmpl.java

--- mybatis/src/main/java/com/study/mybatis/member/service/MemberServicelmpl.java
+++ mybatis/src/main/java/com/study/mybatis/member/service/MemberServicelmpl.java
@@ -4,17 +4,28 @@ import org.apache.ibatis.session.SqlSession;
 
 import com.study.mybatis.common.template.Template;
 import com.study.mybatis.member.dao.MemberDao;
+import com.study.mybatis.member.vo.Member;
 
 public class MemberServicelmpl implements MemberService {
-	
-	private MemberDao mDao = new MemberDao();
-	
-	@Override
-	public int checkId(String userId) {
-		SqlSession sqlSession = Template.getSqlSession();
-		int checkid = mDao.checkId(sqlSession, userId);
-		sqlSession.close();
-		return checkid;
-	}
-	
+   private MemberDao mDao = new MemberDao();
+   
+   @Override
+   public int checkId(String userId) {
+      SqlSession sqlSession = Template.getSqlSession();
+      int checkid = mDao.checkId(sqlSession, userId);
+      sqlSession.close();
+      return checkid;
+   }
+
+   public int insertMember(Member m) {
+      SqlSession sqlSession = Template.getSqlSession();
+      int result = mDao.insertMember(sqlSession, m);
+      
+      if(result>0) {
+         sqlSession.commit();
+      }
+      sqlSession.close();
+      return result;
+   }
+
 }
