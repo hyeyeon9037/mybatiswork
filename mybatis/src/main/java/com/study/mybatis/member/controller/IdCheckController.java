c67d8301137169ad7d97b1f1712b174358502e8f
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-31 16:22:08 +0900

    Update IdCheckController.java

--- mybatis/src/main/java/com/study/mybatis/member/controller/IdCheckController.java
+++ mybatis/src/main/java/com/study/mybatis/member/controller/IdCheckController.java
@@ -7,13 +7,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.study.mybatis.member.service.MemberServicelmpl;
+
 
 public class IdCheckController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String userId = request.getParameter("id");
+		
+		int result = new MemberServicelmpl().checkId(userId);
+		if(result > 0) {
+			response.getWriter().print("idN");
+		}else {
+			response.getWriter().print("idY");
+		}
 	}
 
 }
