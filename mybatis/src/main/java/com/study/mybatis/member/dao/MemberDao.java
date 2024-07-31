07b5b7f6998338632cc534dd7c5e6575b7e568e1
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-31 16:22:15 +0900

    Update MemberDao.java

--- mybatis/src/main/java/com/study/mybatis/member/dao/MemberDao.java
+++ mybatis/src/main/java/com/study/mybatis/member/dao/MemberDao.java
@@ -4,6 +4,6 @@ import org.apache.ibatis.session.SqlSession;
 
 public class MemberDao {
 	public int checkId(SqlSession sqlSession, String userId) {
-		return sqlSession.selectOne("", userId);
+		return sqlSession.selectOne("memberMapper.checkId", userId);
 	}
 }
