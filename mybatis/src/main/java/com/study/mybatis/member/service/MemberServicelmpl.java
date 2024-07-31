a61e0914be4b5c7dfc55955bae7175a297acc788
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-31 15:32:06 +0900

    Create MemberServicelmpl.java

---
+++ mybatis/src/main/java/com/study/mybatis/member/service/MemberServicelmpl.java
@@ -0,0 +1,20 @@
+package com.study.mybatis.member.service;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.study.mybatis.common.template.Template;
+import com.study.mybatis.member.dao.MemberDao;
+
+public class MemberServicelmpl implements MemberService {
+	
+	private MemberDao mDao = new MemberDao();
+	
+	@Override
+	public int checkId(String userId) {
+		SqlSession sqlSession = Template.getSqlSession();
+		int checkid = mDao.checkId(sqlSession, userId);
+		sqlSession.close();
+		return checkid;
+	}
+
+}
