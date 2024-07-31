6f20b3ed661cc201633aabf33d7351bb407e7c85
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-31 16:22:05 +0900

    Update Template.java

--- mybatis/src/main/java/com/study/mybatis/common/template/Template.java
+++ mybatis/src/main/java/com/study/mybatis/common/template/Template.java
@@ -8,26 +8,23 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class Template {
-   public static SqlSession getSqlSession() {
-      SqlSession sqlSession = null;
-      
-      // SqlSession 을 생성하기 위해 => SqlSessionFactory 필요
-      // SqlSessionFactory => SqlSessionFactoryBuilder 필요
-      
-      String resource = "/mybatis-config.xml";
-      
-      try {
-         InputStream stream = Resources.getResourceAsStream(resource);
-         sqlSession = new SqlSessionFactoryBuilder().build(stream).openSession(false);
-         // openSession(false) : 기본값 false
-         //                  false(기본값) => 개발자가 commit 해줌
-         //                  true => 자동커밋
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-      return sqlSession;
-      
-      
-      
-   }
+	public static SqlSession getSqlSession() {
+		SqlSession sqlSession = null;
+		
+		// SqlSession을 생성하기 위해 => SqlSessionFactory 필요
+		// SqlSessionFactory => SqlSessionFactoryBuilder 필요
+		
+		String resource = "/mybatis-config.xml";
+		
+		try {
+			InputStream stream = Resources.getResourceAsStream(resource);
+			sqlSession = new SqlSessionFactoryBuilder().build(stream).openSession(false);
+			// openSession() : 자동커밋의 여부
+			//                 false(기본값)  => 개발자가 commit해줌
+			//                 true => 자동커밋
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sqlSession;
+	}
 }
