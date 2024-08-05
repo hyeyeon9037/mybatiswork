888070055c67bf160e47df4f6a8675dca5b0abcd
hyeyeon9037 <hyeyeon9037@naver.com>
2024-08-05 10:51:34 +0900

    Update LoginController.java

--- mybatis/src/main/java/com/study/mybatis/member/controller/LoginController.java
+++ mybatis/src/main/java/com/study/mybatis/member/controller/LoginController.java
@@ -14,10 +14,14 @@ public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Member m = new Member();
+		Member m = new Member(); // 멤버 선언
+		
+		// m이라는 멤버 객체에 userId와 userPwd만 담아준다.
 		m.setUserId(request.getParameter("userId"));
 		m.setUserPwd(request.getParameter("userPwd"));
 		
+		// m이라는 객체를 썼는데 왜 loginUser을 썼냐면 id와 Pwd만 쓰기를 위함
+		// loginUser는 실제로 데이터베이스에 있는지, 없는지 검증하기위해서 객체선언
 		Member loginUser = new MemberServicelmpl().loginMember(m);
 		//loginMember(m) 은 MemberService에 선언을 해줘야 오류가 뜨지않는다.
 		
